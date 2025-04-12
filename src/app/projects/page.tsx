8cf4e4b1f704028c8dd33c3d3b1544ce03362e74
shatng10969 <1019016072@qq.com>
2025-04-13 01:36:59 +0800

    修改电脑端视频播放器配置，添加完整功能

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -412,7 +412,7 @@ export default function ProjectsPage() {
                       <iframe
                         src={`https://player.bilibili.com/player.html?bvid=${project.bvid}&page=1&high_quality=1&danmaku=0&autoplay=${(isMobile && expandedVideo === project.bvid) || (!isMobile && hoveredVideo === project.bvid) ? 1 : 0}&direction=0&showinfo=1&controls=1&disablekb=0&enable_ssl=1&playsinline=1`}
                         className={`w-full h-full ${!isMobile && hoveredVideo !== project.bvid ? 'hidden' : ''}`}
-                        allow="autoplay; fullscreen"
+                        allow="autoplay; fullscreen; picture-in-picture"
                         sandbox="allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
                         ref={(el) => {
                           if (el) {
